600064b7e31f01381c295b206bacf9f0324b05f6
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 15:02:47 -0300

    Update index.jsx

--- src/pages/Sheet/index.jsx
+++ src/pages/Sheet/index.jsx
@@ -1,4 +1,6 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
 
 import { CharacterDetails } from "../../components/characterDetails/index";
@@ -13,7 +15,19 @@ import ArrowBack from "@mui/icons-material/ArrowBack";
 import Container from "@mui/material/Container";
 
 export const Sheet = () => {
+  const [characterSheet, setCharacterSheet] = useState([]);
   const navigate = useNavigate();
+  const { character_id } = useParams();
+
+  useEffect(() => {
+    fetchCharacter();
+  }, [character_id]);
+
+  const fetchCharacter = async () => {
+    const response = await api.get(`/characters/${character_id}`);
+    setCharacterSheet(response.data);
+    console.log(response.data);
+  };
 
   return (
     <Container>
@@ -31,13 +45,73 @@ export const Sheet = () => {
         </div>
         <div className={styles.grid}>
           <div className={styles.leftGridContainer}>
-            <CharacterDetails />
-            <Attributes />
-            <Skills />
+            <CharacterDetails
+              details={{
+                name: characterSheet?.name,
+                age: characterSheet?.age,
+                occupation: characterSheet?.occupation,
+                archetype: characterSheet?.archetype,
+              }}
+            />
+            <Attributes
+              details={{
+                forca: characterSheet?.forca,
+                intelecto: characterSheet?.intelecto,
+                presenca: characterSheet?.presenca,
+                agilidade: characterSheet?.agilidade,
+                vigor: characterSheet?.vigor,
+              }}
+            />
+            <Skills
+              details={{
+                atletismo: characterSheet?.atletismo,
+                atualidades: characterSheet?.atualidades,
+                ciencia: characterSheet?.ciencia,
+                diplomacia: characterSheet?.diplomacia,
+                enganacao: characterSheet?.enganacao,
+                fortitude: characterSheet?.fortitude,
+                furtividade: characterSheet?.furtividade,
+                intimidacao: characterSheet?.intimidacao,
+                investigacao: characterSheet?.investigacao,
+                luta: characterSheet?.luta,
+                medicina: characterSheet?.medicina,
+                ocultismo: characterSheet?.ocultismo,
+                percepcao: characterSheet?.percepcao,
+                pilotagem: characterSheet?.pilotagem,
+                pontaria: characterSheet?.pontaria,
+                prestidigitacao: characterSheet?.prestidigitacao,
+                profissao: characterSheet?.profissao,
+                reflexos: characterSheet?.reflexos,
+                religiao: characterSheet?.religiao,
+                tatica: characterSheet?.tatica,
+                tecnologia: characterSheet?.tecnologia,
+                vontade: characterSheet?.vontade,
+              }}
+            />
           </div>
           <div className={styles.rightGridContainer}>
-            <Vitals />
-            <Defenses />
+            <Vitals
+              details={{
+                max_life: characterSheet?.max_life,
+                curr_life: characterSheet?.curr_life,
+                max_san: characterSheet?.max_san,
+                curr_san: characterSheet?.curr_san,
+                max_eff: characterSheet?.max_eff,
+                curr_eff: characterSheet?.curr_eff,
+              }}
+            />
+            <Defenses
+              details={{
+                defesa: characterSheet?.defesa,
+                fisico: characterSheet?.fisico_res,
+                balistico: characterSheet?.balistico_res,
+                sangue: characterSheet?.sangue_res,
+                morte: characterSheet?.morte_res,
+                medo: characterSheet?.medo_res,
+                conhecimento: characterSheet?.conhecimento_res,
+                energia: characterSheet?.energia_res,
+              }}
+            />
             <Inventory />
           </div>
         </div>
