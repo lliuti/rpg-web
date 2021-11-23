d34e21e35c54d71d64743905cb7045b18a283166
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 02:23:36 -0300

    Create index.jsx

---
+++ src/components/attributes/index.jsx
@@ -0,0 +1,59 @@
+import styles from "./styles.module.scss";
+
+import TextField from "@mui/material/TextField";
+
+const Attributes = () => {
+  return (
+    <div className={styles.attributesContainer}>
+      <h1>Atributos</h1>
+      <div className={styles.gridTwoItems}>
+        <TextField
+          id="strInput"
+          label="Forca"
+          defaultValue="Forca"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          id="intInput"
+          label="Intelecto"
+          defaultValue="Intelecto"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+      <div className={styles.gridTwoItems}>
+        <TextField
+          id="preInput"
+          label="Presenca"
+          defaultValue="Presenca"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          id="dexInput"
+          label="Agilidade"
+          defaultValue="Agilidade"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+      <div className={styles.gridTwoItems}>
+        <TextField
+          id="vigInput"
+          label="Vigor"
+          defaultValue="Vigor"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Attributes;
