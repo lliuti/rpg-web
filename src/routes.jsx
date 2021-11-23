8df81f22f5f3d3b8d1b14e2405d80717d3b952b2
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 10:57:09 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,22 +1,44 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/index";
-import Register from "./pages/Register/index";
-import Main from "./pages/Main/index";
-import CreateCharacter from "./pages/CreateCharacter/index";
-import Sheet from "./pages/Sheet/index";
+import { useAuth } from "./contexts/useAuth";
+import { Login } from "./pages/Login/index";
+import { Register } from "./pages/Register/index";
+import { Main } from "./pages/Main/index";
+import { CreateCharacter } from "./pages/CreateCharacter/index";
+import { Sheet } from "./pages/Sheet/index";
+import { NotFound } from "./pages/NotFound/index";
 
-const Router = () => {
+export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<ProtectedMain />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/create-character" element={<CreateCharacter />} />
-        <Route path="/characters/xpto" element={<Sheet />} />
+        <Route
+          path="/create-character"
+          element={<ProtectedCreateCharacter />}
+        />
+        <Route path="/characters/xpto" element={<ProtectedSheet />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default Router;
+const ProtectedMain = () => {
+  const context = useAuth();
+
+  return context.signed ? <Main /> : <Login />;
+};
+
+const ProtectedCreateCharacter = () => {
+  const context = useAuth();
+
+  return context.signed ? <CreateCharacter /> : <Login />;
+};
+
+const ProtectedSheet = () => {
+  const context = useAuth();
+
+  return context.signed ? <Sheet /> : <Login />;
+};
