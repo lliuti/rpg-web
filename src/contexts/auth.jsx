805d70bf2852a9221238ca9451c58f692bcc5425
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 10:57:15 -0300

    Create auth.jsx

---
+++ src/contexts/auth.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../services/api";
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+
+    if (storedToken) {
+      setToken(storedToken);
+      api.defaults.headers.Authorization = `Bearer ${storedToken}`;
+    }
+  }, []);
+
+  const Login = async (username, password) => {
+    const response = await api.post("/login", {
+      username,
+      password,
+    });
+
+    setToken(response.data);
+    localStorage.setItem("token", response.data);
+    api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+  };
+
+  const Logout = async () => {
+    setToken("");
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ signed: Boolean(token), Login, Logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
