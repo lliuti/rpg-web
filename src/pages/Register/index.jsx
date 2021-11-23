cd5e668e8e7ea036336fbaf840861caf72504776
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 10:57:25 -0300

    Update index.jsx

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,37 +1,83 @@
+import { useState } from "react";
+import { useAuth } from "../../contexts/useAuth";
 import { useNavigate } from "react-router-dom";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import styles from "./styles.module.scss";
+import { api } from "../../services/api";
 
-const Register = () => {
+export const Register = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
+  const context = useAuth();
+
+  const handleCreateAccount = async () => {
+    if (name == "" && username == "" && email == "" && password == "") {
+      return;
+    }
+
+    const response = await api.post("/players", {
+      name,
+      username,
+      email,
+      password,
+    });
+
+    context.Login(username, password);
+    navigate("/");
+  };
+
   return (
     <Container>
       <div className={styles.formBox}>
         <h1>Registrar</h1>
         <div className={styles.gridTwoItems}>
-          <TextField id="nameInput" label="Nome" variant="outlined" />
-          <TextField id="usernameInput" label="Usuario" variant="outlined" />
+          <TextField
+            id="nameInput"
+            label="Nome"
+            variant="outlined"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="usernameInput"
+            label="Usuario"
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className={styles.gridTwoItems}>
-          <TextField id="emailInput" label="Email" variant="outlined" />
+          <TextField
+            id="emailInput"
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <TextField
             id="passwordInput"
             label="Senha"
             variant="outlined"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className={styles.gridTwoItems}>
           <Button variant="text" onClick={() => navigate("/login")}>
             LOGIN
           </Button>
-          <Button variant="contained">REGISTRAR</Button>
+          <Button onClick={handleCreateAccount} variant="contained">
+            REGISTRAR
+          </Button>
         </div>
       </div>
     </Container>
   );
 };
-
-export default Register;
