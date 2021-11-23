214537f06c2c73f247a026c0b34e8c87c6364857
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 10:57:31 -0300

    Update index.jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import { useAuth } from "../../contexts/useAuth";
 import { useNavigate } from "react-router-dom";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import styles from "./styles.module.scss";
 
-const Login = () => {
+export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
+  const context = useAuth();
+
+  const handleLogin = () => {
+    context.Login(username, password);
+    navigate("/");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleLogin();
+    }
+  };
 
   return (
     <Container>
@@ -13,12 +30,21 @@ const Login = () => {
         <h1>Login</h1>
 
         <div className={styles.gridTwoItems}>
-          <TextField id="usernameInput" label="Usuario" variant="outlined" />
+          <TextField
+            id="usernameInput"
+            label="Usuario"
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <TextField
             id="usernamePassword"
             label="Senha"
             variant="outlined"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyPress={(e) => handleKeyPress(e)}
           />
         </div>
 
@@ -26,11 +52,11 @@ const Login = () => {
           <Button variant="text" onClick={() => navigate("/register")}>
             CRIAR CONTA
           </Button>
-          <Button variant="contained">ENTRAR</Button>
+          <Button onClick={handleLogin} variant="contained">
+            ENTRAR
+          </Button>
         </div>
       </div>
     </Container>
   );
 };
-
-export default Login;
