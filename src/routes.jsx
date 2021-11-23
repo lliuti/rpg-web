fdd7e204ff9b88cd62e610c064ff51df31c9f932
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 01:31:55 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/index";
 import Register from "./pages/Register/index";
 import Main from "./pages/Main/index";
+import CreateCharacter from "./pages/CreateCharacter/index";
+import Sheet from "./pages/Sheet/index";
 
 const Router = () => {
   return (
@@ -10,6 +12,8 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/create-character" element={<CreateCharacter />} />
+        <Route path="/characters/xpto" element={<Sheet />} />
       </Routes>
     </BrowserRouter>
   );
