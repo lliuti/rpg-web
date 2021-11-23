9af5e81fe750a338a1951e5e79bf0c770f743cbb
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 15:02:54 -0300

    Update index.jsx

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,20 @@
-import { useAuth } from "../../contexts/useAuth";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/useAuth";
 import styles from "./styles.module.scss";
 
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import ExitToApp from "@mui/icons-material/ExitToApp";
+import { api } from "../../services/api";
 
 export const Main = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetchMyCharacters();
+  }, []);
+
   const characterImage = "https://pbs.twimg.com/media/EYFiJSNWAAEFSi9.png";
   const navigate = useNavigate();
   const context = useAuth();
@@ -14,6 +22,13 @@ export const Main = () => {
   const handleLogout = () => {
     context.Logout();
     navigate("/login");
+  };
+
+  const fetchMyCharacters = () => {
+    setTimeout(async () => {
+      const response = await api.get("/characters");
+      setCharacters(response.data);
+    }, 150);
   };
 
   return (
@@ -30,28 +45,31 @@ export const Main = () => {
             </Button>
           </div>
           <div className={styles.charactersGrid}>
-            <div
-              onClick={() => navigate("/characters/xpto")}
-              className={styles.characterBox}
-            >
-              <div className={styles.row}>
-                <span>nome:</span>
-                <h1>Elizabeth Webber</h1>
+            {characters?.map((character) => (
+              <div
+                onClick={() => navigate(`/characters/${character.id}`)}
+                className={styles.characterBox}
+                key={character.id}
+              >
+                <div className={styles.row}>
+                  <span>nome:</span>
+                  <h1>{character.name}</h1>
+                </div>
+                <img src={character.picture_url} alt="Character picture" />
+                <div className={styles.row}>
+                  <span>ocupacao:</span>
+                  <h1>{character.occupation}</h1>
+                </div>
+                <div className={styles.row}>
+                  <span>arquétipo:</span>
+                  <h1>{character.archetype}</h1>
+                </div>
+                <div className={styles.row}>
+                  <span>exp:</span>
+                  <h1>{character.exp}</h1>
+                </div>
               </div>
-              <img src={characterImage} alt="Character picture" />
-              <div className={styles.row}>
-                <span>ocupacao:</span>
-                <h1>Investigadora Forense</h1>
-              </div>
-              <div className={styles.row}>
-                <span>arquétipo:</span>
-                <h1>Especialista</h1>
-              </div>
-              <div className={styles.row}>
-                <span>exp:</span>
-                <h1>50%</h1>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </Container>
