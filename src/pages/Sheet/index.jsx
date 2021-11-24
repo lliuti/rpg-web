fc0d524baf751ba8bc1a11519961d6bcf4c1ff4f
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 12:29:25 -0300

    Update index.jsx

--- src/pages/Sheet/index.jsx
+++ src/pages/Sheet/index.jsx
@@ -9,15 +9,27 @@ import { Skills } from "../../components/skills/index";
 import { Vitals } from "../../components/vitals/index";
 import { Defenses } from "../../components/defenses/index";
 import { Inventory } from "../../components/inventory/index";
+import { Attacks } from "../../components/attacks/index";
 
 import Button from "@mui/material/Button";
 import ArrowBack from "@mui/icons-material/ArrowBack";
 import Container from "@mui/material/Container";
 
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 export const Sheet = () => {
   const [characterSheet, setCharacterSheet] = useState([]);
+  const [expanded, setExpanded] = useState(false);
   const navigate = useNavigate();
   const { character_id } = useParams();
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   useEffect(() => {
     fetchCharacter();
@@ -116,6 +128,53 @@ export const Sheet = () => {
             />
             <Inventory />
           </div>
+        </div>
+
+        <div className={styles.attacksRituals}>
+          <Accordion
+            expanded={expanded === "panel1"}
+            onChange={handleChange("panel1")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                Ataques
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Agir ofensivamente com uma arma/luta
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Attacks />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel2"}
+            onChange={handleChange("panel2")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                Rituais
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Conjurar rituais através do Outro Lado
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Donec placerat, lectus sed mattis semper, neque lectus feugiat
+                lectus, varius pulvinar diam eros in elit. Pellentesque
+                convallis laoreet laoreet.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </div>
     </Container>
