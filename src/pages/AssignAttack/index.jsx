d0cb45b16f37204433e162c07790fe74faab98d4
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-25 11:22:12 -0300

    Update index.jsx

--- src/pages/AssignAttack/index.jsx
+++ src/pages/AssignAttack/index.jsx
@@ -25,7 +25,7 @@ export const AssignAttack = () => {
   }, []);
 
   const fetchCharacters = async () => {
-    const response = await api.get("/characters");
+    const response = await api.get("/dashboard/characters");
     setCharacters(response.data);
   };
 
