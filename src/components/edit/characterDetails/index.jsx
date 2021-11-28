5932ca84a61a3d604fc9729f798c9521676b2f51
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-28 19:56:28 -0300

    Update index.jsx

--- src/components/edit/characterDetails/index.jsx
+++ src/components/edit/characterDetails/index.jsx
@@ -5,20 +5,60 @@ import { api } from "../../../services/api";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const EditCharacterDetails = ({ details }) => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [occupation, setOccupation] = useState("");
+  const [archetype, setArchetype] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState();
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="error"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
 
   const handleUpdateSheet = async () => {
-    const response = await api.put(`/characters/${details.character_id}/info`, {
-      name,
-      age,
-      occupation,
-    });
-
-    console.log(response.data);
+    setLoading(true);
+    try {
+      await api.put(`/characters/${details.character_id}/info`, {
+        name,
+        age: parseInt(age),
+        occupation,
+        archetype,
+      });
+      setLoading(false);
+    } catch (err) {
+      setOpen(true);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -29,6 +69,11 @@ export const EditCharacterDetails = ({ details }) => {
     setName(details.name);
     setAge(details.age);
     setOccupation(details.occupation);
+    setArchetype(details.archetype);
+  };
+
+  const handleArchetypeChange = (event) => {
+    setArchetype(event.target.value);
   };
 
   return (
@@ -38,8 +83,7 @@ export const EditCharacterDetails = ({ details }) => {
         <TextField
           id="nameInput"
           label="Nome"
-          value={name}
-          defaultValue={details.name}
+          value={name ?? ""}
           onChange={(e) => setName(e.target.value)}
           InputProps={{
             readOnly: false,
@@ -50,8 +94,7 @@ export const EditCharacterDetails = ({ details }) => {
         <TextField
           id="ageInput"
           label="Idade"
-          value={age}
-          defaultValue={details.age}
+          value={age ?? ""}
           onChange={(e) => setAge(e.target.value)}
           InputProps={{
             readOnly: false,
@@ -63,8 +106,7 @@ export const EditCharacterDetails = ({ details }) => {
         <TextField
           id="occupationInput"
           label="Ocupacao"
-          value={occupation}
-          defaultValue={details.occupation}
+          value={occupation ?? ""}
           onChange={(e) => setOccupation(e.target.value)}
           InputProps={{
             readOnly: false,
@@ -72,20 +114,47 @@ export const EditCharacterDetails = ({ details }) => {
           }}
         />
 
-        <TextField
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">Arquétipo</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={archetype ?? ""}
+            label="Arquétipo"
+            onChange={handleArchetypeChange}
+          >
+            <MenuItem value={"Combatente"}>Combatente</MenuItem>
+            <MenuItem value={"Especialista"}>Especialista</MenuItem>
+            <MenuItem value={"Ocultista"}>Ocultista</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/* <TextField
           id="archetypeInput"
           label="Arquétipo"
-          value={details.archetype || ""}
+          value={details.archetype}
           defaultValue={details.archetype}
           InputProps={{
-            readOnly: true,
+            readOnly: false,
             autoFocus: true,
           }}
-        />
+        /> */}
       </div>
-      <Button onClick={handleUpdateSheet} variant="outlined">
+      <LoadingButton
+        loading={loading}
+        onClick={handleUpdateSheet}
+        variant="outlined"
+      >
         ATUALIZAR
-      </Button>
+      </LoadingButton>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        color="error"
+        message="Não foi possível atualizar as informacoes pessoais."
+        action={action}
+      />
     </div>
   );
 };
