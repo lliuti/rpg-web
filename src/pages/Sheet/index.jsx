d663c37168341c8a256da11fcc69f509269ed206
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-30 18:21:46 -0300

    Update index.jsx

--- src/pages/Sheet/index.jsx
+++ src/pages/Sheet/index.jsx
@@ -13,8 +13,11 @@ import { Attacks } from "../../components/attacks/index";
 import { Rituals } from "../../components/rituals/index";
 
 import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
 import ArrowBack from "@mui/icons-material/ArrowBack";
 import Container from "@mui/material/Container";
+
+import BorderColorIcon from "@mui/icons-material/BorderColor";
 
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -24,17 +27,47 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
 
 export const Sheet = () => {
   const [characterSheet, setCharacterSheet] = useState([]);
   const [expanded, setExpanded] = useState(false);
-  const navigate = useNavigate();
-  const { character_id } = useParams();
   const [openBackdrop, setOpenBackdrop] = useState(false);
 
-  const handleCloseBackdrop = () => {
-    setOpenBackdrop(false);
+  const [annotationsDialogOpen, setAnnotationsDialogOpen] = useState(false);
+  const [backgroundMultiline, setBackgroundMultiline] = useState("");
+  const [annotationsMultiline, setAnnotationsMultiline] = useState("");
+
+  const [annotationsLoading, setAnnotationsLoading] = useState(false);
+  const [getNotesLoading, setGetNotesLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const { character_id } = useParams();
+
+  const handleOpenAnnotationsDialog = async () => {
+    setGetNotesLoading(true);
+    try {
+      const response = await api.get(`/characters/${character_id}/notes`);
+      setBackgroundMultiline(response.data[0].background_text);
+      setAnnotationsMultiline(response.data[0].note_text);
+      setGetNotesLoading(false);
+      setAnnotationsDialogOpen(true);
+    } catch (err) {
+      console.log(err);
+      setGetNotesLoading(false);
+    }
   };
+
+  const handleCloseAnnotationsDialog = () => setAnnotationsDialogOpen(false);
+
+  const handleCloseBackdrop = () => setOpenBackdrop(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -57,19 +90,136 @@ export const Sheet = () => {
     }
   };
 
+  const handleSaveAnnotations = async () => {
+    try {
+      await api.put(`/characters/${character_id}/notes`, {
+        background: backgroundMultiline,
+        annotations: annotationsMultiline,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container>
       <div className={styles.sheetContainer}>
         <div className={styles.topContainer}>
-          <Button
-            variant="outlined"
-            onClick={() => navigate("/")}
-            color="inherit"
-            startIcon={<ArrowBack />}
-          >
-            VOLTAR
-          </Button>
-          <h1>Ficha de Personagem</h1>
+          <div className={styles.leftArea}>
+            <Button
+              variant="outlined"
+              onClick={() => navigate("/")}
+              color="inherit"
+              startIcon={<ArrowBack />}
+            >
+              VOLTAR
+            </Button>
+            <h1>Ficha de Personagem</h1>
+          </div>
+          <div className={styles.leftArea}>
+            <LoadingButton
+              loading={getNotesLoading}
+              variant="outlined"
+              color="inherit"
+              startIcon={<BorderColorIcon />}
+              loadingPosition="start"
+              onClick={handleOpenAnnotationsDialog}
+            >
+              ANOTACOES
+            </LoadingButton>
+            <Dialog
+              fullScreen
+              open={annotationsDialogOpen}
+              onClose={handleCloseAnnotationsDialog}
+              sx={{ background: "#fff" }}
+            >
+              <AppBar sx={{ position: "relative" }}>
+                <Toolbar>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={handleCloseAnnotationsDialog}
+                    aria-label="close"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                  <Typography
+                    sx={{ ml: 2, flex: 1 }}
+                    variant="h6"
+                    component="div"
+                  >
+                    Anotacoes
+                  </Typography>
+                  <LoadingButton
+                    loading={annotationsLoading}
+                    color="inherit"
+                    onClick={handleSaveAnnotations}
+                  >
+                    salvar
+                  </LoadingButton>
+                </Toolbar>
+              </AppBar>
+              <div className={styles.annotationsContainer}>
+                <Accordion
+                  expanded={expanded === "panel2"}
+                  onChange={handleChange("panel2")}
+                  sx={{ mb: 5 }}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header"
+                  >
+                    <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                      Anotacoes
+                    </Typography>
+                    <Typography sx={{ color: "text.secondary" }}>
+                      Lembretes, anotacoes, informacoes gerais.
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <TextField
+                      id="annotations-multiline-flexible"
+                      label="Anotacoes"
+                      fullWidth={true}
+                      rows={15}
+                      multiline
+                      defaultValue={annotationsMultiline}
+                      onBlur={(e) => setAnnotationsMultiline(e.target.value)}
+                    />
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel1"}
+                  onChange={handleChange("panel1")}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                  >
+                    <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                      História
+                    </Typography>
+                    <Typography sx={{ color: "text.secondary" }}>
+                      Contexto, explicacoes e história do personagem
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <TextField
+                      id="background-multiline-flexible"
+                      label="História"
+                      fullWidth={true}
+                      rows={15}
+                      multiline
+                      defaultValue={backgroundMultiline}
+                      onBlur={(e) => setBackgroundMultiline(e.target.value)}
+                    />
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </Dialog>
+          </div>
         </div>
         <div className={styles.grid}>
           <div className={styles.leftGridContainer}>
